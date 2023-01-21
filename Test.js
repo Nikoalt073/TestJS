497a7dd7cb2bf961b15570613b41f155c98fcb95
Nikoalt073 <119976044+Nikoalt073@users.noreply.github.com>
2023-01-21 13:25:25 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,3 +1,2 @@
 /// Test.js
-(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); window.addEventListener('load', function () { eruda.init() }) })();
-eruda.add(erudaCode);
+(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda-code@2.1.0"; document.body.appendChild(script); window.addEventListener('load', function () { eruda.init(); eruda.add(erudaCode); }) })();
