849deba5338d67503d340ac11d33a1aa2fd6bbbd
Nikoalt073 <119976044+Nikoalt073@users.noreply.github.com>
2023-01-21 12:10:14 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,4 +2,3 @@
 var src = 'https://cdn.jsdelivr.net/npm/eruda';
 document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
 document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
-alert("Loaded");
