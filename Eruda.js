1027284780272d9ce5a3aced856c28462963385b
Nikoalt073 <119976044+Nikoalt073@users.noreply.github.com>
2023-01-21 13:42:49 -0500

    Update and rename Test.js to Eruda.js

---
+++ Eruda.js
@@ -0,0 +1,2 @@
+/// Eruda.js
+(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); window.addEventListener('load', function () { eruda.init() }) })();
--- Test.js
+++
@@ -1,2 +0,0 @@
-/// Test.js
-(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; script.src="//cdn.jsdelivr.net/npm/eruda-code@2.1.0"; document.body.appendChild(script); window.addEventListener('load', function () { eruda.init() }) })();
